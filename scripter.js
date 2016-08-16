11977c2c02de740b23070d651c9bc1dae92bade8
Anulfo <noaharkling@gmail.com>
2016-08-16 14:54:11 -0500

    another container for DOM and got all the data loaded

--- scripter.js
+++ scripter.js
@@ -1,23 +1,53 @@
 $(document).ready(function(){
-
+var categories = [];
+var types = [];
+var products = [];
     var catData = function() {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: "categories.json"
-        }).done(function(data) {
-            resolve(data);
-        }).fail(function(xhr, status, error){
-            reject(error);
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: "categories.json"
+            }).done(function(cat) {
+                resolve(cat);
+            }).fail(function(xhr, status, error){
+                reject(error);
+            })
         })
-    })
-}
+    }
 
-    catData().then(function(categories) {
-        console.log(categories);
-    });
+    var typData = function(cat) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: "types.json", data: cat
+            }).done(function(typ){
+                resolve(typ);
+            }).fail(function(xhr, status, error) {
+                reject(error);
+            })
+        })
+    }    
+
+    var prodData = function(catandtyp) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: "products.json", data: catandtyp
+            }).done(function(catandtyp){
+                resolve(catandtyp);
+            }).fail(function(xhr, status, error) {
+                reject(error);
+            })
+        })
+    }
+ catData()
+  .then(function(cat) {
+    categories = cat;
+    return typData();
+  })
+  .then(function(typ) {
+    types = typ;
+    return prodData(typ);
+  })
+  .then(function (prod) {
+    products = prod.products;
+    console.log(categories, types, products);
+  });
 })
-  // categoriesXHR()
-  // .then(function(data1) {
-  //   categories = data1;
-  //   return typesXHR();
-  // })
